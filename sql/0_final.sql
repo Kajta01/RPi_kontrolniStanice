a4e589fee628422d2cc2405768acbdc3b6194e5f
Kamca <kama.chmelarova@gmail.com>
2021-02-17 19:09:08 +0000

    sql

--- sql/0_final.sql
+++ sql/0_final.sql
@@ -1,28 +1,32 @@
 
 SELECT 
-    ID, 
+    ID_Ucastnik, 
     Prezdivka,  
     Zivoty,
     CasNaTrase,
     CekaciCas,
     RUN,
-    CP, 
-    SEC_TO_TIME(TIME_TO_SEC( CasNaTrase )- TIME_TO_SEC( CekaciCas ) - ((Zivoty+RUN+CP)*60) ) as VyslednyCas
+    CP,
+    RUN_CP,
+    Vyloucen,
+    SEC_TO_TIME(TIME_TO_SEC( CasNaTrase )- TIME_TO_SEC( CekaciCas ) - ((ZivotyBody + RUN+CP + RUN_CP)*60) ) as VyslednyCas
 
 
 
 FROM (
   SELECT 
-    V.ID, 
+    V.ID_Ucastnik, 
     V.Prezdivka,
+    V.Vyloucen,
     V.Zivoty,
+    V.ZivotyBody,
     TIMEDIFF(V.Cil,V.Start) as CasNaTrase,
-    (V.RUN01 + V.RUN02 + V.RUN03 + V.RUN02 + V.RUN05 + V.RUN06 + V.RUN07) as RUN,
-    (V.CP1 + V.CP2 + V.CP3 + V.CP4 + V.CP5) as CP,
-    Time(TIME_FORMAT(SEC_TO_TIME(  TIME_TO_SEC( V.CP1_T )+TIME_TO_SEC( V.CP2_T ) +TIME_TO_SEC( V.CP3_T ) 
-    +TIME_TO_SEC( V.CP4_T )+TIME_TO_SEC( V.CP5_T )) , "%H:%i:%s")) AS CekaciCas
+    (V.RUN_01 + V.RUN_02 + V.RUN_03 + V.RUN_02 + V.RUN_05 + V.RUN_06 + V.RUN_07) as RUN,
+    (V.CP_01 + V.CP_02 + V.CP_03 + V.CP_04 + V.CP_05) as CP,
+    (V.CP_01_RUN + V.CP_02_RUN + V.CP_03_RUN + V.CP_04_RUN +V.CP_05_RUN)  as RUN_CP,
+    Time(TIME_FORMAT(SEC_TO_TIME(  TIME_TO_SEC( V.CP_01_T )+TIME_TO_SEC( V.CP_02_T ) +TIME_TO_SEC( V.CP_03_T ) 
+    +TIME_TO_SEC( V.CP_04_T )+TIME_TO_SEC( V.CP_05_T )) , "%H:%i:%s")) AS CekaciCas
 
   FROM V_000_Vysledky as V
-    where V.Vyloucen = 0
 ) AS x
 order by VyslednyCas
--- sql/1_hlavni.sql
+++ sql/1_hlavni.sql
@@ -1,38 +1,39 @@
 SELECT
-Ucastnik.ID,
+Ucastnik.ID as ID_Ucastnik,
 Ucastnik.Prezdivka,
 Ucastnik.Vyloucen,
-IfNull(ZtraceneZivoty.Pocet * 0_Pravidla.Zivoty,0)  as Zivoty,
+IfNull(ZtraceneZivoty.Pocet,0)  as Zivoty,
+IfNull(ZtraceneZivoty.Pocet * 0_Pravidla.Zivoty,0)  as ZivotyBody,
 Time(V_001_Start.Cas) as Start,
 Time(V_002_Cil.Cas) AS Cil,
 
-IfNull(V_RUN_01.Body, 0) as RUN01,
-IfNull(V_RUN_02.Body, 0) as RUN02,
-IfNull(V_RUN_03.Body, 0) as RUN03,
-IfNull(V_RUN_04.Body, 0) as RUN04,
-IfNull(V_RUN_05.Body, 0) as RUN05,
-IfNull(V_RUN_06.Body, 0) as RUN06,
-IfNull(V_RUN_07.Body, 0) as RUN07,
+IfNull(V_RUN_01.Body, 0) as RUN_01,
+IfNull(V_RUN_02.Body, 0) as RUN_02,
+IfNull(V_RUN_03.Body, 0) as RUN_03,
+IfNull(V_RUN_04.Body, 0) as RUN_04,
+IfNull(V_RUN_05.Body, 0) as RUN_05,
+IfNull(V_RUN_06.Body, 0) as RUN_06,
+IfNull(V_RUN_07.Body, 0) as RUN_07,
 
-IfNull(CP1.Body,0) as CP1,
-IfNull(V_CP_01.Rozdil,0) as CP1_T,
-IfNull(V_CP_01.Body,0) as CP1_RUN,
+IfNull(CP_01.Body,0) as CP_01,
+IfNull(V_CP_01.Rozdil,0) as CP_01_T,
+IfNull(V_CP_01.Body,0) as CP_01_RUN,
 
-IfNull(CP2.Body,0) as CP2,
-IfNull(V_CP_02.Rozdil,0) as CP2_T,
-IfNull(V_CP_02.Body,0) as CP2_RUN,
+IfNull(CP_02.Body,0) as CP_02,
+IfNull(V_CP_02.Rozdil,0) as CP_02_T,
+IfNull(V_CP_02.Body,0) as CP_02_RUN,
 
-IfNull(CP3.Body,0) as CP3,
-IfNull(V_CP_03.Rozdil,0) as CP3_T,
-IfNull(V_CP_03.Body,0) as CP3_RUN,
+IfNull(CP_03.Body,0) as CP_03,
+IfNull(V_CP_03.Rozdil,0) as CP_03_T,
+IfNull(V_CP_03.Body,0) as CP_03_RUN,
 
-IfNull(CP4.Body,0) as CP4,
-IfNull(V_CP_04.Rozdil,0) as CP4_T,
-IfNull(V_CP_04.Body,0) as CP4_RUN,
+IfNull(CP_04.Body,0) as CP_04,
+IfNull(V_CP_04.Rozdil,0) as CP_04_T,
+IfNull(V_CP_04.Body,0) as CP_04_RUN,
 
-IfNull(CP5.Body,0) as CP5,
-IfNull(V_CP_05.Rozdil,0) as CP5_T,
-IfNull(V_CP_05.Body,0) as CP5_RUN
+IfNull(CP_05.Body,0) as CP_05,
+IfNull(V_CP_05.Rozdil,0) as CP_05_T,
+IfNull(V_CP_05.Body,0) as CP_05_RUN
 
 from 0_Pravidla, Ucastnik
 LEFT JOIN V_001_Start on Ucastnik.ID = V_001_Start.ID_Ucastnik
@@ -46,11 +47,11 @@ LEFT JOIN V_RUN_05 on Ucastnik.ID = V_RUN_05.ID_Ucastnik
 LEFT JOIN V_RUN_06 on Ucastnik.ID = V_RUN_06.ID_Ucastnik
 LEFT JOIN V_RUN_07 on Ucastnik.ID = V_RUN_07.ID_Ucastnik
 
-LEFT JOIN CP1 on Ucastnik.ID = CP1.ID_Ucastnik
-LEFT JOIN CP2 on Ucastnik.ID = CP2.ID_Ucastnik
-LEFT JOIN CP3 on Ucastnik.ID = CP3.ID_Ucastnik
-LEFT JOIN CP4 on Ucastnik.ID = CP4.ID_Ucastnik
-LEFT JOIN CP5 on Ucastnik.ID = CP5.ID_Ucastnik
+LEFT JOIN CP_01 on Ucastnik.ID = CP_01.ID_Ucastnik
+LEFT JOIN CP_02 on Ucastnik.ID = CP_02.ID_Ucastnik
+LEFT JOIN CP_03 on Ucastnik.ID = CP_03.ID_Ucastnik
+LEFT JOIN CP_04 on Ucastnik.ID = CP_04.ID_Ucastnik
+LEFT JOIN CP_05 on Ucastnik.ID = CP_05.ID_Ucastnik
 
 LEFT JOIN V_CP_01 on Ucastnik.ID = V_CP_01.ID_Ucastnik
 LEFT JOIN V_CP_02 on Ucastnik.ID = V_CP_02.ID_Ucastnik
